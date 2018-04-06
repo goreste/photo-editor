ec4f14591fe59df90025303f22509693b1959223
Paolo Furlan <paolo_furlan@hotmail.it>
2018-04-06 15:45:18 +0200

    fixes

--- Example/PhotoEditor/Pods/Target Support Files/Pods-PhotoEditor/Pods-PhotoEditor-frameworks.sh
+++ Example/PhotoEditor/Pods/Target Support Files/Pods-PhotoEditor/Pods-PhotoEditor-frameworks.sh
@@ -100,17 +100,6 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Gifu/Gifu.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PhotoEditor/PhotoEditor.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Gifu/Gifu.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PhotoEditor/PhotoEditor.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
-fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
